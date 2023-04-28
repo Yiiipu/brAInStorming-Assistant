80d588e4399f3ef731e2fc38e75fcba0f58db8b7
Yiiipu <ydu7@nd.edu>
2023-04-28 18:46:08 -0400

    Initial Commit

---
+++ brAInstorming/brAInstormingApp.swift
@@ -0,0 +1,17 @@
+//
+//  brAInstormingApp.swift
+//  brAInstorming
+//
+//  Created by YIPU on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct brAInstormingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
